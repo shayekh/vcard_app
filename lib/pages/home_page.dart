93e45071875c5181d318571c22c44fae24834dff
shayekh <shayekhebnemizan@gmail.com>
2024-02-20 02:00:50 +0600

    3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/';
+
   const HomePage({super.key});
 
   @override
@@ -9,10 +10,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home')),
-    );
+        appBar: AppBar(
+          title: const Text("Contact List"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+
+          },
+          shape: CircleBorder(),
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            onTap: (index){
+              setState(() {
+                selectedIndex = index;
+              });
+
+            },
+            currentIndex: selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'All',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: const Center(child: Text('Home')));
   }
 }
