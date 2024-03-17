10f1c2a887d28d681a0df696fc3a7ff4ba4e57c2
shayekh <shayekhebnemizan@gmail.com>
2024-03-18 03:43:59 +0600

    Scan

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ScanPage extends StatefulWidget {
@@ -58,6 +61,21 @@ class _ScanPageState extends State<ScanPage> {
     final xFile = await ImagePicker().pickImage(source: source);
     if (xFile != null) {
       image = xFile.path;
+      final textRecognizer =
+          TextRecognizer(script: TextRecognitionScript.latin);
+      final recognizedText =
+          await textRecognizer.processImage(InputImage.fromFile(File(image)));
+      final tempList = <String>[];
+      for(var block in recognizedText.blocks){
+        for(var line in block.lines){
+          tempList.add(line.text);
+        }
+      }
+
+      setState(() {
+        lines = tempList;
+        isScanOver = true;
+      });
     }
   }
 }
